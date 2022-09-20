3aa3710f668ef45146f547e7efdc5e6ae24aa30f
THE_DARK <106637454+sthevenospina@users.noreply.github.com>
2022-09-20 16:57:33 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    private final static double PI = 3.14;
+    private static int numero1;
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite su primer numero");
+        numero1 = input.nextInt();
+        System.out.println("resultado: " + (numero1 * PI));
+    }
+}
